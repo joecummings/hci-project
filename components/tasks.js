4d3bca8f5b4dbfc10a10ee9a0a0ba1211985cca7
Joe Cummings <jrcummings27@gmail.com>
2018-03-03 17:01:18 -0600

    enter keypress functionality

--- components/tasks.js
+++ components/tasks.js
@@ -63,3 +63,11 @@ function assign() {
 function go() {
   window.location.href = 'manual.html';
 }
+
+document.getElementById("myInput")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("submite").click();
+    }
+});
